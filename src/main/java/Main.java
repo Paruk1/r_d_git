2e0a69da0e5bfab7d7cabf2f55a1b8fcf8ef8f93
Paruk1 <parail200111@gmail.com>
2022-02-15 19:04:50 +0200

    fix - initializing params

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1,b = 2,c = 0,d = 1;
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        int d = 1;
         int summ1,summ2;
 
         summ1 = a + b;

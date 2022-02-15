fdd69d1cd9b8973abd0c93de34c251636bd7dabf
Paruk1 <parail200111@gmail.com>
2022-02-15 15:36:15 +0200

    Comparing and Multiplicity

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1,b = 2,c = 0,d = 1;
+        int summ1,summ2;
+
+        summ1 = a + b;
+        summ2 = c + d;
+        System.out.printf("Результат сравнения: %s \n", compareSumm(summ1,summ2));
+        summ1++; // 4
+        summ2 -= 2; // -1
+        System.out.printf("Результат сравнения x2: %s \n", compareSumm(summ1,summ2));
+        System.out.printf("Хотя бы одна сумма кратна 2: %s", multiplicityTwo(summ1,summ2));
+    }
+
+    public static boolean compareSumm(int a,int b){
+        return a > b;
+    }
+
+    public static boolean multiplicityTwo(int a,int b){
+        return a%2 == 0 || b%2 ==0;
+    }
+}
